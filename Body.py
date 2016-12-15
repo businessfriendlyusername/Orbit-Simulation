5efa2a80358a86172cd104ce233597692f1db3f5
jaredvu <jaredvu@gmail.com>
2016-12-15 14:43:16 -0800

    Fixed Graphs
    Fixed Graphs

--- Body.py
+++ Body.py
@@ -2,7 +2,7 @@ import math
 
 class Body:
     def __init__(self, velocity, radius, mass, orbits=0, pos=0):
-    	self.mass = mass
+    	self.mass = float(mass)
     	if orbits == 0:
 	        # calculate the tangential velocity vector <x,y> based on the velocity (velocity) and the angle (pos)
 	        self.velocity = [math.sin(math.radians(pos)) * velocity, math.cos(math.radians(pos)) * velocity]
--- simulation.py
+++ simulation.py
@@ -13,17 +13,20 @@ scene = display(title='Orbit Sim', width = 1000, height=1000, center=(0,0,0))
 G = 6.66308 * math.pow(10, -11)
 bodies = []
 
-#Time Variables that can be changed
-time = 0
-dt = 86400 / 100
+#-------------------------------------------------------------------------------------
+#Time Variables **that can be changed**
+time = 0 #Starting Time
+dt = 86400/100 #The smaller the dt, the more accurate the orbits are
 years = 1
-final_time = 86400 * 365 * years 
-rate_ani = 10000
+final_time = 86400 * 365.24 * years #Ending Time
+rate_ani = 10000 #Animation Rate.
+#-------------------------------------------------------------------------------------
 
-#Append Bodies to be used in Sim
-bodies.append(Body(0, 0, 1.98855*math.pow(10,30))) # sun
-bodies.append(Body(29800, 149600000000, 5.972*math.pow(10,24), bodies[0], 180)) # earth
-bodies.append(Body(1000, 384400000, 7.347*math.pow(10,22), bodies[1])) # poon
+#Append Bodies to be used in Simulation
+bodies.append(Body(0, 0, 1.98855*math.pow(10,30))) # Sun
+bodies.append(Body(29800, 149600000000, 5.972*math.pow(10,24), bodies[0])) # Earth
+bodies.append(Body(1000, 384400000, 7.347*math.pow(10,22), bodies[1])) # Moon
+
 
 var = [0.0] * len(bodies) * 4
 num_eqs = len(bodies) * 4
@@ -65,60 +68,55 @@ for i in range(0, len(bodies) * 4, 4):
 
 #----------------------------------Graphics-----------------------------------
     #initialize default shapes
-    # radius of sun = 6.95 * 10**8
-    # radius of Earth = 6.37* 10**6
+    # Actual radius of Sun = 6.95 * 10**8
+    # Actual radius of Earth = 6.37* 10**6
 Sun = sphere(pos=(bodies[0].pos[0],bodies[0].pos[1],0), radius=3*10**10, color = color.orange)
+#Draw Earth
 Earth = sphere(pos=(bodies[1].pos[0],bodies[1].pos[1],0), radius=6.37*10**9, material=materials.BlueMarble, make_trail=True)
 Earth.trail_object.color=color.green
 Earth.trail_type = points
 Earth.interval = 10
 Earth.retain=1000
+#Draw Moon
 Moon = sphere(pos=(bodies[2].pos[0],bodies[2].pos[1],0), radius=1*10**9, make_trail=True)
 Moon.trail_object.color=color.white
 Moon.trail_type=points
 Moon.interval=10
 Moon.retain=500
-
 #------------------------------------------------------------------------------
 
 ES_Distance = []
 ME_Distance = []
+gdisplay(title='Distance between Earth and Sun',ymin = 1.49*10**11, ymax = 1.51*10**11, xtitle='Time(Years)',ytitle='Distance(KM)',background=color.white, foreground=color.black)
+
+f1 = gcurve(color=color.cyan)
+gdisplay(title='Distance between Moon and Earth',xtitle='Time(Years)',ytitle='Distance(KM)',background=color.white, foreground=color.black)
+f2 = gcurve(color=color.red)
 while time < final_time:
     var = integrator.integrateRK4(var, dt)
-
     for i in range(0, len(bodies) * 4, 4):
+        #check for accuracy by verifying that the system follows laws of conservation of energy
         rate(rate_ani)
+        #Update the position of the bodies
         bodies[int(i/4)].pos[0] = var[i]
         bodies[int(i/4)].pos[1] = var[i+1]
-        #Moon Distance scaling-----------------------
+        #Moon Distance Scaling-----------------------
+        #   This is so the Moon can be visible within Simulation. Only changes where it is drawn not the actual position.
         scale = 50
         scaleX = scale*(bodies[1].pos[0]-bodies[2].pos[0])
         scaleY = scale*(bodies[1].pos[1]-bodies[2].pos[1])
         #Drawing-------------------------------------
+        #   Update the drawn position of the bodies
         Sun.pos = (bodies[0].pos[0],bodies[0].pos[1],0)
         Earth.pos = (bodies[1].pos[0],bodies[1].pos[1],0)
         Moon.pos = (bodies[2].pos[0]+scaleX,bodies[2].pos[1]+scaleY,0)
         #Record Distance-----------------------------
-        ES_Distance.append(sqrt(((bodies[0].pos[0]-bodies[1].pos[0])**2+(bodies[0].pos[1]-bodies[1].pos[1])**2)))
-        ME_Distance.append(sqrt(((bodies[1].pos[0]-bodies[2].pos[0])**2+(bodies[1].pos[1]-bodies[2].pos[1])**2)))
+        ES_Distance = sqrt(((bodies[0].pos[0]-bodies[1].pos[0])**2+(bodies[0].pos[1]-bodies[1].pos[1])**2)) #Earth-Sun Distance
+        ME_Distance = sqrt(((bodies[1].pos[0]-bodies[2].pos[0])**2+(bodies[1].pos[1]-bodies[2].pos[1])**2)) #Moon-Earth Distance
         #Update Velocity-----------------------------
         bodies[int(i/4)].velocity[0] = var[i+2]
         bodies[int(i/4)].velocity[1] = var[i+3]
-        
-
+        mass = bodies[int(1/4)].mass/1000
+        f1.plot(pos=((time/(86400 * 365.24)),ES_Distance))
+        f2.plot(pos=((time/(86400 * 365.24)),ME_Distance))
     time += dt
-
-#--------------------------------Graphing------------------------------------
-gdisplay(title='Distance between Earth and Sun',xtitle='Time',ytitle='Distance(KM)',background=color.white, foreground=color.black)
-f1 = gcurve(color=color.cyan)
-gdisplay(title='Distance between Moon and Earth',xtitle='Time',ytitle='Distance(KM)',background=color.white, foreground=color.black)
-f2 = gcurve(color=color.red)
-
-for x in range(len(ES_Distance)):
-    f1.plot(pos=(x,ES_Distance[x]))
-
-
-for x in range(len(ME_Distance)):
-    f2.plot(pos=(x,ME_Distance[x]))
-
-
